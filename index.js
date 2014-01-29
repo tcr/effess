5a36866c5e620d1453547aeb8cde6f612fe4ebc0
Tim Cameron Ryan <id@timryan.org>
2014-01-29 12:56:42 -0500

    Augment fs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require('fs')
 var path = require('path')
 
+module.exports = exports = fs;
+
 // inflateSymlinks => should follow symlinks?
 // excludeHiddenUnix => don't match files with leading periods
 // filter == filter(file, dir) returns bool
